fcc645ef6cd839cb117a8497948f4ab47b41132f
Linwei Zhang <lewiszlw520@gmail.com>
2023-01-31 13:26:33 +0800

    Setup basic scene

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,70 @@
+use bevy::prelude::{*, shape::CapsuleUvProfile};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .run();
+}
+
+pub fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // 地面
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 500000.0 })),
+        material: materials.add(Color::rgb(0.95, 0.87, 0.88).into()),
+        ..default()
+    });
+
+    // cube
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..default()
+    });
+
+    // player
+    commands.spawn(PbrBundle {
+        // TODO 换成圆柱体
+        mesh: meshes.add(Mesh::from(shape::Capsule {
+            radius: 0.2,
+            rings: 0,
+            depth: 0.5,
+            latitudes: 16,
+            longitudes: 32,
+            uv_profile: CapsuleUvProfile::Aspect,
+        })),
+        material: materials.add(Color::PINK.into()),
+        transform: Transform::from_xyz(0.0, 1.5, 0.0),
+        ..default()
+    });
+
+    // cube
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+        transform: Transform::from_xyz(3.0, 0.5, 0.0),
+        ..default()
+    });
+
+    // 方向光
+    commands.spawn(DirectionalLightBundle {
+        directional_light: DirectionalLight {
+            illuminance: 15000.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(2.0, 10.0, 8.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+
+    // camera
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-5.0, 8.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
 }
